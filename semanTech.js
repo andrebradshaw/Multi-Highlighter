ce13686588cee227d7571a8f710c858ef525acd6
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-09-15 23:11:41 -0400

    Create semanTech.js

---
+++ semanTech.js
@@ -0,0 +1,18 @@
+var similarTech = [{
+key:"jvm",
+val: ["JVM","Clojure","groovy","jruby","jython","kotlin","scala","beenshell","Ceylon","JudoScript","rascaIMPL"],
+},{
+key:"CSS Framework",
+val: ["CSS Framework","Material Framework","Leaf","Materialize","Essence","Bootstrap","Semantic UI","Foundation","Baseguide","Siimple","Responsive Cat","Sculpt","Turret","Concise CSS","Blueprint","UIkit","Modest Grid","Schema","Metro UI","Responsive Grid System","YAML"]
+},{
+key:"javascript framework.",
+val: []
+},{
+key:"javascript",
+val: ["JS","React","ReactJS","Angular","AngularJS","Vue.JS","vueJS","node","node.js","nodeJS","JQuery","TypeScript","Babel.js","CoffeeScript","Dart","Emscripten","Google Closure Compiler","Google Web Toolkit","Morfik","AtScript","Opa","Nim","Haxe","ClojureScript","WebSharper"]
+},{
+key: "javascript transcompilers",
+val: ["TypeScript","Babel.js","CoffeeScript","Dart","Emscripten","Google Closure Compiler","Google Web Toolkit","Morfik","AtScript","Opa","Nim","Haxe","ClojureScript","WebSharper"]
+}
+
+];
